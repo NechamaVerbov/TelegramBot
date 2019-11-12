de09777a8d31f2b7987494befa7bc1878ad21bdc
tirtzara <tirtzashahar@gmail.com>
2019-11-13 01:46:18 +0200

    stage 2.2.2

--- bot.py
+++ bot.py
@@ -1,7 +1,3 @@
-import secret_settings
-
-print(secret_settings.BOT_TOKEN)
-
 """
 First, a few callback functions are defined. Then, those functions are passed to
 the Dispatcher and registered at their respective places.
@@ -9,67 +5,155 @@ Then, the bot is started.
 Usage:
 ...
 """
+import logging
+import bot_keyboards
+import re
+import secret_settings
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackQueryHandler)
 from telegram import Update
+from model import add_parent_to_db, add_child_to_db, child_name_to_parent, is_parent, child_id_to_parent
 from telegram.ext import (CallbackContext)
-import bot_keyboard
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def get_info_from(key: str):
+    name, parent_id = key.split('-')
+    return name, parent_id
 
 
 def start(update: Update, context: CallbackContext):
-    pass
+    chat_id = update.effective_chat.id
+    key = context.args
+    if key:
+        logger.info(f"> Start child chat #{chat_id}")
+        name, parent_id = get_info_from(key[0])
+        add_child_to_db(name, parent_id, chat_id)
+        child_id_to_parent(parent_id, chat_id)
+        home_child(update, context, chat_id)
+    else:
+        logger.info(f"> Start parent chat #{chat_id}")
+        add_parent_to_db(update, chat_id)
+        context.bot.send_message(chat_id=chat_id, text=f"Welcome! {update.message.from_user['first_name']}")
+        add_child(update, context, chat_id)
 
 
-def home_parent(update: Update, context: CallbackContext):
-    pass
+def home_parent(update: Update, chat_id, context: CallbackContext):
+    bot_keyboards.main_parent_keyboard(update, chat_id, context)
 
 
 def home_child(update: Update, context: CallbackContext, chat_id):
-    bot_keyboard.main_child_keyboard(update, chat_id, context)
+    bot_keyboards.main_child_keyboard(update, chat_id, context)
 
 
-def add_child(update: Update, context: CallbackContext):
-    pass
+def add_child(update, context, chat_id):
+    context.bot.send_message(chat_id=chat_id, text=f"Enter your child's name:")
+
+
+def adding_child(update, context, child_name):
+    chat_id = update.effective_chat.id
+    logger.info(f"> Adding {child_name} to parent #{chat_id}")
+
+    child_name_to_parent(chat_id, child_name)
+    context.bot.send_message(chat_id=chat_id, text=f"Send your child this link to join this bot: "
+                                                   f"\n https://t.me/teachild_bot?start={child_name}-{str(chat_id)}")
+    home_parent(update, chat_id, context)
 
 
 def assign_task(update: Update, context: CallbackContext):
-    pass
+    bot_keyboards.choose_child_for_task(update, context, update.effective_chat.id)
 
 
-def get_report(update: Update, context: CallbackContext):
-    pass
-
-
-def start_ques1(update: Update, context: CallbackContext):
-    pass
-
-
-def get_answer(update: Update, context: CallbackContext):
-    pass
-
-
-def check_answer(update: Update, context: CallbackContext):
-    pass
+def start_task_one(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    level = 0  # task list- rather the first one that's not completed
+    context.bot.send_message(chat_id=chat_id, text=f"You are solving Math - level {level}.\n Good luck!!!")
+    amount_questions = 7  # length of questions in that level
+    # get task
+    context.bot.send_photo(chat_id=chat_id, photo=open('screenshots/shopping-list-bot-1.png', 'rb'),
+                           caption=f"Question #1/{amount_questions}")
+    bot_keyboards.child_in_task_keyboard(update, chat_id, context)
 
 
 def next_task(update: Update, context: CallbackContext):
-    pass
+    chat_id = update.effective_chat.id
+    current_ques = 2  # get current task + 1
+    amount_questions = 7  # length of questions in that level
+    # get task
+    context.bot.send_photo(chat_id=chat_id, photo=open('screenshots/shopping-list-bot-1.png', 'rb'),
+                           caption=f"Question #1/{amount_questions}")
 
 
-def done_task(update: Update, context: CallbackContext):
-    pass
+def callback_query_handler_choose_child(update, context):
+    parent_id = update.effective_chat.id
+    context.user_data['choose_child'] = update.callback_query.data
+    bot_keyboards.choose_level_task(update, context)
 
 
-def generate_report(update: Update, context: CallbackContext):
-    pass
+def callback_query_handler_choose_level(update, context):
+    chat_id = update.effective_chat.id
+    child_level = update.callback_query.data
+    bot_keyboards.model.create_task_in_db(chat_id, context.user_data['choose_child'], child_level.replace('x', ''))
+    context.bot.send_message(chat_id=chat_id, text="task send")
+    context.bot.send_message(chat_id=context.user_data['choose_child'], text=f"Hi, you have a new assignment 😃")
 
 
-def show_child_tasks(update, context):
-    pass
-
-
-def help(update: Update, context: CallbackContext):
-    pass
+def respond(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    text = update.message.text
+    if text == 'Add child':
+        add_child(update, context, chat_id)
+    elif text == 'Assign task':
+        assign_task(update, context)
+    elif text == 'Get report':
+        pass
+    elif text == 'Start Task':
+        start_task_one(update, context)
+    elif text == 'Show Tasks':
+        pass
+    elif is_parent(chat_id):
+        adding_child(update, context, text)
+    else:
+        next_task(update, context)
 
 
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(secret_settings.BOT_TOKEN, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # log all errors
+    dispatcher.add_error_handler(error)
+
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    help_handler = CommandHandler('help', help)
+    dispatcher.add_handler(help_handler)
+
+    echo_handler = MessageHandler(Filters.text, respond)
+    dispatcher.add_handler(echo_handler)
+
+    dispatcher.add_handler(CallbackQueryHandler(callback_query_handler_choose_child, pattern=re.compile(r'\d')))
+
+    dispatcher.add_handler(CallbackQueryHandler(callback_query_handler_choose_level, pattern=re.compile(r'x\d+')))
+
+    logger.info("* Start polling...")
+    updater.start_polling()  # Starts polling in a background thread.
+    updater.idle()  # Wait until Ctrl+C is pressed
+    logger.info("* Bye!")
+
+
+if __name__ == '__main__':
+    main()
--- model.py
+++ model.py
@@ -1,1 +1,119 @@
-# YOUR BOT LOGIC/STORAGE/BACKEND FUNCTIONS HERE
+import random
+import feedparser
+import dict_questions
+from pymongo.mongo_client import MongoClient
+from datetime import datetime
+
+client = MongoClient()
+db = client.get_database("Teachild")
+db.drop_collection('Parent')
+db.drop_collection('Child')
+db.drop_collection('Task')
+parent_collection = db.get_collection('Parent')
+child_collection = db.get_collection('Child')
+task_collection = db.get_collection('Task')
+
+'''
+#0-39
+dict_questions.dict_questions_math_level1
+#0-29
+dict_questions.dict_questions_math_level2
+'''
+NUMBER_Q_IN_TASK = 10
+
+
+def make_task_for_level(dictt: dict) -> dict:
+    return dict(zip(range(NUMBER_Q_IN_TASK), random.choices(dictt, k=NUMBER_Q_IN_TASK)))
+
+
+def send_task(level) -> dict:
+    if int(level) == 1:
+        return make_task_for_level(dict_questions.dict_questions_math_level1)
+    if int(level) == 2:
+        return make_task_for_level(dict_questions.dict_questions_math_level2)
+
+
+def check_answer(name_img_q: str, answer) -> bool:
+    return int(name_img_q.split('=')[1].split('.')[0]) == int(answer)
+
+
+def add_parent_to_db(update, chat_id):
+    p = {
+        'parent_id': chat_id,
+        'name': update.message.from_user['first_name'],
+        'children_ids': [],  # a list of children id's
+        'children_names': []
+    }
+
+    response = parent_collection.replace_one({'parent_id': chat_id}, p, upsert=True)
+
+
+def add_child_to_db(name, p_id, chat_id):
+    c = {
+        'child_id': chat_id,
+        'name': name,
+        'parent_id': p_id,
+        'tasks': [],  # a list of dictionaries
+        'current_task': 0
+    }
+    response = child_collection.replace_one({'child_id': chat_id}, c, upsert=True)
+
+
+def add_task_to_db(c_id, p_id, level, dictionary):
+    t = {
+        'child_id': c_id,
+        'parent_id': p_id,
+        'level': level,
+        'status': False,
+        'date': datetime.now(),
+        'task_dict': dictionary,
+        'data_time_start': datetime.datetime(),
+        'data_time_finishing': datetime.datetime(),
+        'current_task': 0
+    }
+    response = task_collection.insert_one(t)
+
+
+def get_date_task_send(task_id):
+    task = db.collection.find({'_id': task_id})
+    d = feedparser.parse(task['date'])
+    return d.strftime("%d/%m/%Y %H:%M:%S")
+
+
+def child_name_to_parent(chat_id, child_name):
+    parent_collection.update({'parent_id': chat_id}, {'$push': {'children_names': child_name}})
+
+
+def child_id_to_parent(parent_id, child_id):
+    parent_collection.update({'parent_id': int(parent_id)}, {'$push': {'children_ids': child_id}})
+
+
+def get_list_child_id(chat_id_parent):
+    return parent_collection.find({'parent_id': chat_id_parent})[0]['children_ids']
+
+
+def get_list_child_name(chat_id_parent):
+    return parent_collection.find({'parent_id': chat_id_parent})[0]['children_names']
+
+
+def is_parent(chat_id):
+    return True if parent_collection.find({'parent_id': chat_id}).count() > 0 else False
+
+
+def get_current_task(child_id):
+    return child_collection.find({'child_id': child_id})[0]['current_task']
+
+
+def create_task_in_db(parent_id, child_id, level):
+    dictionary = send_task(level)
+    add_task_to_db(child_id, parent_id, level, dictionary)
+    print(child_id, level)
+
+
+def get_report(parent_id, child_id):
+    pass
+
+
+def get_num_of_level():
+    # כמה רמות יש לי
+    return int(2)
